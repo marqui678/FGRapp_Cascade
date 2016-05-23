febc7aeae2bde18efadb403eb2028028509d701b
Jia <jia@KendraGsMacBook.home>
2016-05-23 13:11:26 -0700

    Fix layout of map search on iOS and some other iOS related bugs.

--- app/controllers/search.js
+++ app/controllers/search.js
@@ -1,5 +1,6 @@
 // Arguments passed into this controller can be accessed via the `$.args` object directly or:
 var args = $.args;
+Ti.UI.setBackgroundColor('#43B02A');
 
 $.searchBar.addEventListener('return', function(e) {
 	searchLocation();
